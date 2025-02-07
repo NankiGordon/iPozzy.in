34296053dc578f2ef333e4eb331c915d976028e7
Sabelo Gordon <nankigordon@gmail.com>
2025-02-07 15:10:07 +0200

    blade 1st

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -5,10 +5,10 @@
                 <p class="text-muted text-center">
                     {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
                 </p>
-    
+
                 <form method="POST" action="{{ route('password.confirm') }}">
                     @csrf
-    
+
                     <!-- Password -->
                     <div class="mb-3">
                         <label for="password" class="form-label">{{ __('Password') }}</label>
@@ -18,7 +18,7 @@
                             <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
                     </div>
-    
+
                     <!-- Confirm Button -->
                     <div class="d-grid">
                         <button type="submit" class="btn btn-primary">
