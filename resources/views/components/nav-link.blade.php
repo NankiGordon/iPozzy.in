23ebc30e12d764acb1aca89d48e564dd292356ef
Sabelo Gordon <nankigordon@gmail.com>
2025-02-07 15:37:06 +0200

    still working on nav Bar

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,21 +1,16 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand d-flex align-items-center" href="https://ipozzy.in/">
-            <img src="{{ asset('img/lolo.jpg') }}" alt="iPozzy Logo" height="80" class="rounded-circle me-2">
-            <span>iPozzy</span>
+        <a class="navbar-brand" href="https://ipozzy.in/">
+            <img src="{{ asset('img/lolo.jpg') }}" alt="iPozzy Logo" height="80" class="rounded-circle">
+            iPozzy
         </a>
-
-
-        <!-- Toggler Button for mobile view -->
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <!-- Collapsible content for mobile view -->
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Navbar links right after the logo -->
-            <ul class="navbar-nav d-flex flex-row ms-auto mb-2 mb-lg-0">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item">
                     <a class="nav-link" href="#">About Us</a>
                 </li>
@@ -25,8 +20,6 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#">Pricing</a>
                 </li>
-            </ul>
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 @guest
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="{{ route('register') }}">Register</a>
