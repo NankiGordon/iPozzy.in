bf3b7910ddc5ad4277b0588ee6739e816386fb8d
Sabelo Gordon <nankigordon@gmail.com>
2025-02-08 11:26:02 +0200

    added vite

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,6 +7,10 @@
     <title>iPozzy</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <!-- Vite Generated CSS -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 </head>
 
 <body>
@@ -20,8 +24,6 @@
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
 
-    <!-- Include Custom App.js Compiled by Laravel Mix -->
-    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 
 </html>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: [
+        'resources/js/app.js',  // Your main JS file
+        'resources/css/app.css', // Your main CSS file
+      ],
+      refresh: true,  // Enables auto-refresh when files change
+    }),
+  ],
 });
