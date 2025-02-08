5fb4a678b98ce3570a9fa746a77cf832b6c7a513
Sabelo Gordon <nankigordon@gmail.com>
2025-02-08 12:04:53 +0200

    add custom.js

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,7 +10,6 @@
 
     <!-- Vite Generated CSS -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-
 </head>
 
 <body>
@@ -23,6 +22,9 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
+
+    <!-- Include Custom JS from Public Folder -->
+    <script src="{{ asset('custom.js') }}"></script> <!-- Path to your custom.js in the public folder -->
 
 </body>
 
