56ecd7225f407e67f7bef3bf4d718a0efa9860c6
Sabelo Gordon <nankigordon@gmail.com>
2025-02-08 12:34:57 +0200

    updated user profiles table

--- database/migrations/2025_02_08_101204_create_user_profiles_table.php
+++ database/migrations/2025_02_08_101204_create_user_profiles_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('profiles', function (Blueprint $table) {
+        Schema::create('user_profiles', function (Blueprint $table) {
             $table->id();
             $table->string('phone');
             $table->string('address1');
@@ -29,6 +29,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('profiles');
+        Schema::dropIfExists('user_profiles');
     }
 };
