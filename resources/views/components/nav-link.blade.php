89e0dfa2843a8cde21bd24e5480162209c008a7c
Sabelo Gordon <nankigordon@gmail.com>
2025-02-08 09:51:51 +0200

    changed user profile modal size

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -261,7 +261,7 @@
 
 <!-- User Profile Modal -->
 <div class="modal fade" id="profileModal" tabindex="-1" aria-labelledby="profileModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-scrollable">
+    <div class="modal-dialog modal-xl">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="profileModalLabel">User Profile</h5>
