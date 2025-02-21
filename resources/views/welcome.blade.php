d182475809beb6c04c13699d3a749d02b6214dcb
Sabelo Gordon <nankigordon@gmail.com>
2025-02-21 11:12:56 +0200

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,25 +1,28 @@
 <x-guest-layout>
     @if($listings->count())
-        <div class="container">
-            <h2>Latest Listings</h2>
-            <div class="row">
-                @foreach($listings as $listing)
-                    <div class="col-md-4">
-                        <div class="card mb-3">
-                            @if(!empty($listing->images) && is_array($listing->images))
-                                <img src="{{ asset('storage/' . $listing->images[0]) }}" class="card-img-top" alt="Property Image">
-                            @endif
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $listing->property_name }}</h5>
-                                <p class="card-text">{{ $listing->description }}</p>
-                                <p><strong>Price:</strong> R{{ number_format($listing->price, 2) }}</p>
-                                <p><strong>Location:</strong> {{ $listing->suburb }}, {{ $listing->city }}</p>
-                            </div>
-                        </div>
+                <div class="container">
+                    <h2>Latest Listings</h2>
+                    <div class="row">
+                        @foreach($listings as $listing)
+                                                <div class="col-md-4">
+                                                    <div class="card mb-3">
+                                                        @php
+                                                            $images = json_decode($listing->images, true);
+                                                        @endphp
+                                                    @if (!empty($images) && is_array($images))
+                                                        <img src="{{ asset('storage/' . $images[0]) }}" class="card-img-top" alt="Property Image">
+                                                    @endif
+                                                        <div class="card-body">
+                                                            <h5 class="card-title">{{ $listing->property_name }}</h5>
+                                                            <p class="card-text">{{ $listing->description }}</p>
+                                                            <p><strong>Price:</strong> R{{ number_format($listing->price, 2) }}</p>
+                                                            <p><strong>Location:</strong> {{ $listing->suburb }}, {{ $listing->city }}</p>
+                                                        </div>
+                                                    </div>
+                                              </div>
+                        @endforeach
                     </div>
-                @endforeach
-            </div>
-        </div>
+                </div>
     @else
         <p>No listings available.</p>
     @endif
